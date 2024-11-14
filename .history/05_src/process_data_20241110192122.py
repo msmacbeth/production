c63ee54dda153478c8434a0a56233c711e4d4d32
msmacbeth <ionamacbeth@gmail.com>
2024-11-14 01:50:50 -0500

    remove unnecessary files from tracking

--- .history/05_src/process_data_20241110192122.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-# Load variables
-load_dotenv()
-
-# Verify paths 
-# if not all([os.getenv("PRICE_DATA"), os.getenv("FEATURES_DATA"), os.getenv("TICKERS")]):
-    raise ValueError("One or more environment variables are not set. Check your .env file.")
