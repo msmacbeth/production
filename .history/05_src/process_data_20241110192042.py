8f4359275e968705ee26f92fe6194414210987aa
msmacbeth <ionamacbeth@gmail.com>
2024-11-14 03:43:06 -0500

    Assignment-1 Finalized

---
+++ .history/05_src/process_data_20241110192042.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+
+# Verify that the paths are loaded correctly
+if not all([os.getenv("PRICE_DATA"), os.getenv("FEATURES_DATA"), os.getenv("TICKERS")]):
+    raise ValueError("One or more environment variables are not set. Check your .env file.")
